445018375e819e0104190ac02852961544280954
Devanshu <devgoyal952@gmail.com>
2024-12-16 13:38:19 +0530

    Created and imported Performance component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Leaderboard from "../src/Leaderboard/Leaderboard";
 import Header from "./Header/Header";
+import Performance from "./Performance/Performance";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Leaderboard />
+      <Performance />
     </div>
   );
 }
---
+++ src/Performance/Performance.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Performance.css";
+
+const Performance = () => {
+  return <div></div>;
+};
+
+export default Performance;
