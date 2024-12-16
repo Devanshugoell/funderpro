bc341fb7d04ce0686ded0d737c4ff28ae83c7edf
Devanshu <devgoyal952@gmail.com>
2024-12-16 13:03:48 +0530

    imported header section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Leaderboard from "../src/Leaderboard/Leaderboard";
+import Header from "./Header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Leaderboard />
     </div>
   );
