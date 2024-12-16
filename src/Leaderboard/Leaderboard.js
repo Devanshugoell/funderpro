3421b00193d3c37f275fe87a7850edd7d620a819
Devanshu <devgoyal952@gmail.com>
2024-12-16 13:34:59 +0530

    leaderBoard completed

--- src/Leaderboard/Leaderboard.js
+++ src/Leaderboard/Leaderboard.js
@@ -3,40 +3,62 @@ import { dummyData } from "../dummyData";
 import "./Leaderboard.css";
 
 const Leaderboard = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState();
 
   //useEffect will gets render when component gets mounted
   useEffect(() => {
-    setData(dummyData);
+    setTimeout(() => {
+      setData(dummyData);
+    }, 1000);
   }, []);
 
-  return (
-    <>
-      <section className="tableSection">
-        <table className="table">
-          <caption className="table-caption">Top 10 Traders</caption>
-          <thead>
-            <tr>
-              <th>#Rank</th>
-              <th>Name</th>
-              <th>Return %</th>
-              <th>$ Profit</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((item) => (
-              <tr key={item.rank}>
-                <td>{item.rank}</td>
-                <td>{item.name}</td>
-                <td>{item.return} %</td>
-                <td>${item.profit}</td>
+  //Conditional rendering
+
+  if (data) {
+    return (
+      <>
+        <section className="tableSection">
+          <table className="table">
+            <caption className="table-caption">Top 10 Traders</caption>
+            <thead>
+              <tr>
+                <th>#Rank</th>
+                <th>Name</th>
+                <th>Return %</th>
+                <th>$ Profit</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {data.map((item) => (
+                <tr key={item.rank}>
+                  <td>{item.rank}</td>
+                  <td>{item.name}</td>
+                  <td>{item.return} %</td>
+                  <td>${item.profit}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <section className="tradersBottom">
+            <p>
+              This is a collection of the top 10 Funded FunderPro Traders who
+              are eligible for payouts. We have chosen to only display the top
+              10 for motivational purposes!
+            </p>
+            <p id="updatedTime">Last Update: less than a minute ago</p>
+          </section>
+        </section>
+      </>
+    );
+  } else {
+    return (
+      <section className="tableSection">
+        <div className="spinner">
+          <div className="spin"></div>
+        </div>
       </section>
-    </>
-  );
+    );
+  }
 };
 
 export default Leaderboard;
