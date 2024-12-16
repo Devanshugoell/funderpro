6ee70ddc3f6e4604cd73f8b193195f8b6a13390d
Devanshu <devgoyal952@gmail.com>
2024-12-16 20:42:58 +0530

    use useState for toggle functionality

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import FunderProImg from "./FunderPro-Logo-removebg-preview.png";
 
 const Header = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleNav = () => {
+    setIsActive((prevState) => !prevState); // Toggle the state
+  };
+
   return (
     <div>
       <nav>
         <img src={FunderProImg} alt="FunderProImg" className="FunderProImg" />
-        <section className="navLinks">
+        <section
+          id="nav-links"
+          className={`navLinks ${isActive ? "active" : ""}`}
+        >
           <p>Leaderboard</p>
           <p>Competitions</p>
           <button>Sign Up</button>
         </section>
+        <a href="#" className="nav-toggle" id="nav-toggle" onClick={toggleNav}>
+          <i className="fas  fa-bars"></i>
+        </a>
       </nav>
       <h1 className="heading">FunderPro Leaderboard</h1>
     </div>
