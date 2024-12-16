090091cf9209b5d09047bf46957f962c114baf4a
Devanshu <devgoyal952@gmail.com>
2024-12-16 11:43:58 +0530

    added dummyData

---
+++ src/dummyData.js
@@ -0,0 +1,62 @@
+export const dummyData = [
+  {
+    rank: 1,
+    name: "Ishaq m.",
+    return: 4.42,
+    profit: 441.65,
+  },
+  {
+    rank: 2,
+    name: "David W.",
+    return: 3.36,
+    profit: 3360.93,
+  },
+  {
+    rank: 3,
+    name: "Donzell S.",
+    return: 1.31,
+    profit: 328.38,
+  },
+  {
+    rank: 4,
+    name: "Alan A.",
+    return: 1.26,
+    profit: 125.65,
+  },
+  {
+    rank: 5,
+    name: "Gian Delfino T.",
+    return: 1.07,
+    profit: 537.34,
+  },
+  {
+    rank: 6,
+    name: "Gaetano S.",
+    return: 1.03,
+    profit: 515.57,
+  },
+  {
+    rank: 7,
+    name: "Guido V.",
+    return: 1.01,
+    profit: 503.94,
+  },
+  {
+    rank: 8,
+    name: "Timo K.",
+    return: 0.96,
+    profit: 1928.56,
+  },
+  {
+    rank: 9,
+    name: "Coste R.",
+    return: 0.92,
+    profit: 924.24,
+  },
+  {
+    rank: 10,
+    name: "Jessica U.",
+    return: 0.85,
+    profit: 425.68,
+  },
+];
